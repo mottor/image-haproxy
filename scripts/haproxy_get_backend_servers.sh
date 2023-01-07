a0048a25c51bc0d0213ea95877a0c5bb7a357c7d
Aleksandr Bakulin <bakulin@lpmotor.ru>
2023-01-07 18:43:24 +0500

    added haproxy_get_backend_servers.sh

---
+++ scripts/haproxy_get_backend_servers.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# https://cbonte.github.io/haproxy-dconv/1.7/management.html
+
+WANTED_BACKEND="${1:-}"
+
+if [ "$WANTED_BACKEND" == "" ]; then
+  echo "ERROR: Need 1st argument = backend name"
+  exit 1
+fi
+
+# Get state from HAproxy
+STATE=$( echo "show servers state" | socat stdio /var/lib/haproxy/admin.sock )
+
+RES=""
+
+echo "$STATE" | while read -r LINE; do
+    # Remove unwanted lines
+    [ "$LINE" == "1" ] && continue
+    ( echo "$LINE" | grep -Eq "^# " ) && continue
+
+    BACKEND_NAME=$( echo $LINE | cut -d' ' -f 2 )
+    [ "$BACKEND_NAME" != "$WANTED_BACKEND" ] && continue
+
+    SRV_NAME=$( echo $LINE | cut -d' ' -f 4 )
+    SRV_ADDR=$( echo $LINE | cut -d' ' -f 5 )
+
+    RES="${RES}${SRV_NAME}@${SRV_ADDR};"
+done
+
+echo "$RES" | sed -E 's/[;]+$//g'
